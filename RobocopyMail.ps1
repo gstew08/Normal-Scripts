53a12c6ab96184af6b43b5974391de95c86814be
gstew08 <ceasura@hotmail.com>
2018-03-02 11:01:52 +0000

    Create RobocopyMail.ps1
    copies a directory to remote source and sends an email of the result.

---
+++ RobocopyMail.ps1
@@ -0,0 +1,29 @@
+$Dest = "\path\of\destination"
+Net use $Dest /u:name $Pass
+
+if(test-path $Dest){
+$Log = "C:\robocopylogs\Backup.log" 
+$File = if(-not(test-path $Log)){ 
+New-Item $Log -ItemType file 
+if (test-path $log){ 
+robocopy $Source $Dest /mir /R:0 /Log:$Log 
+} 
+} 
+Else{ 
+robocopy $Source $Dest /mir /R:0 /Log:$Log} 
+$Source = "\path\to\source"  
+$Test = Get-content $Log | Select -last 15 
+$Pass = "Archer2"
+
+$File  
+if(test-path $log){ 
+Set-content -path $log -value $source 
+Add-Content -path $log -value $dest 
+Add-content -path $log -Value $Test 
+if(test-path $log){
+$contents = Get-Content -Path $Log
+Send-MailMessage -To "name@mail.com" -From "robocopy@mail.com" -SmtpServer "smtpaddress" -Subject "Backup Copy" -Body "$contents"
+Net use $Dest /Delete
+}
+} 
+}
